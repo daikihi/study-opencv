5a7c2280e5fea5cd97dd5e4472929c4c02c7eaeb
daiki <daikihi+github@gmail.com>
2024-12-30 11:37:02 +0900

    それっぽいのが取れている

--- ocr_test/ocr_test01.py
+++ ocr_test/ocr_test01.py
@@ -4,11 +4,10 @@
 # I can not upload my sample photo because I dont have license to that documents.
 # please use your own images.
 
-
-
 import os
 import cv2
 import pytesseract
+import numpy as np
 
 def load_from_file():
     # Tesseractの実行ファイルのパスを指定
@@ -20,7 +19,8 @@ def load_from_file():
 
     # 画像を読み込む
     _image = cv2.imread('private_sample04.jpg')
-    return _image
+    _template = cv2.imread("private_target_sample1.jpg")
+    return _image, _template
 
 def prepare_common_image_processing(image):
     # グレースケールに変換
@@ -31,19 +31,40 @@ def prepare_common_image_processing(image):
     _, thresh_image = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     return thresh_image
 
+# 空行を削除する
+def remove_blank_lines(text):
+    lines = text.split('\n')
+    non_empty_lines = [line for line in lines if line.strip() != '']
+    return '\n'.join(non_empty_lines)
+
 
 #  ここより下がMain 関数に当たる、最初に実行される場所
-image = load_from_file() # イメージファイルを取得する 
+image, template = load_from_file() # イメージファイルを取得する 
 
 thresh_image = prepare_common_image_processing(image) # 基本的な前処理をする
 
 
 # 今はここを実験中
-contours_image, _ = cv2.findContours(thresh_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contours_image, _ = cv2.findContours(thresh_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+matching_result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+threshold = 0.55
+locations = np.where(matching_result >= threshold)   # 一致する部分の位置を取得
+template_height, template_width = template.shape[:2] # template 画像の大きさを取得
+for pt in zip(*locations[::-1]):
+    cv2.rectangle(image, pt, (pt[0] + template_width, pt[1] + template_height), (255, 255, 255), -1)
+
+# cv2.imshow('Detected', image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 
 # Tesseractで画像からテキストを抽出
-text = pytesseract.image_to_string(thresh_image, lang='jpn')
+text = pytesseract.image_to_string(image, lang='jpn')
+
+from string import digits
+table = str.maketrans("", "", digits)
+non_digits_text = text.translate(table)
+non_empty_lines = remove_blank_lines(non_digits_text)
 
 print("Extracted Text:")
-print(text)
+print(non_empty_lines)
