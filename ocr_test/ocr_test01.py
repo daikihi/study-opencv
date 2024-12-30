38fb9f7164b8d2ed31cb1262120e94298e211692
daiki <daikihi+github@gmail.com>
2024-12-31 08:49:44 +0900

    イメージのファイル名を複数指定できるように変更した

--- ocr_test/ocr_test01.py
+++ ocr_test/ocr_test01.py
@@ -8,8 +8,9 @@ import os
 import cv2
 import pytesseract
 import numpy as np
+import re
 
-def load_from_file():
+def load_from_file(file_name):
     # Tesseractの実行ファイルのパスを指定
     pytesseract.pytesseract.tesseract_cmd = r'/opt/homebrew/bin/tesseract'
 
@@ -18,7 +19,7 @@ def load_from_file():
 
 
     # 画像を読み込む
-    _image = cv2.imread('private_sample04.jpg')
+    _image = cv2.imread(filename=file_name)
     _template = cv2.imread("private_target_sample1.jpg")
     return _image, _template
 
@@ -43,33 +44,42 @@ def remove_after_characters(text):
     modified_text = re.sub(r'\(.*', '', text)
     modified_text = re.sub(r'\[.*', '', modified_text)
     modified_text = re.sub(r'\=.*', '', modified_text)
+    pattern = re.compile(r'[ァ-ヴー\n]+')
+    modified_text = ''.join(pattern.findall(modified_text))
     return modified_text
 
 #  ここより下がMain 関数に当たる、最初に実行される場所
-image, template = load_from_file() # イメージファイルを取得する 
 
-thresh_image = prepare_common_image_processing(image) # 基本的な前処理をする
+_images_file_names = ["private_sample04.jpg", "private_sample05.jpg"]
 
+for _file_name in _images_file_names:
+    image, template = load_from_file(_file_name) # イメージファイルを取得する 
 
-# 今はここを実験中
-contours_image, _ = cv2.findContours(thresh_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-matching_result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
-threshold = 0.55
-locations = np.where(matching_result >= threshold)   # 一致する部分の位置を取得
-template_height, template_width = template.shape[:2] # template 画像の大きさを取得
-for pt in zip(*locations[::-1]):
-    cv2.rectangle(image, pt, (pt[0] + template_width, pt[1] + template_height), (255, 255, 255), -1)
-
-# Tesseractで画像からテキストを抽出
-text = pytesseract.image_to_string(image, lang='jpn')
-
-from string import digits
-table = str.maketrans("", "", digits)
-non_digits_text = text.translate(table)
-non_empty_lines = remove_blank_lines(non_digits_text)
-removed_texts = remove_after_characters(non_empty_lines)
-non_empty_lines = remove_blank_lines(removed_texts)
+    thresh_image = prepare_common_image_processing(image) # 基本的な前処理をする
 
 
-print("Extracted Text:")
-print(non_empty_lines)
+    # 今はここを実験中
+    contours_image, _ = cv2.findContours(thresh_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    matching_result = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+    threshold = 0.57
+    locations = np.where(matching_result >= threshold)   # 一致する部分の位置を取得
+    template_height, template_width = template.shape[:2] # template 画像の大きさを取得
+    for pt in zip(*locations[::-1]):
+        cv2.rectangle(image, pt, (pt[0] + template_width, pt[1] + template_height), (255, 255, 255), -1)
+
+    # Tesseractで画像からテキストを抽出
+    text = pytesseract.image_to_string(image, lang='jpn')
+
+    from string import digits
+    table = str.maketrans("", "", digits)
+    non_digits_text = text.translate(table)
+    non_empty_lines = remove_blank_lines(non_digits_text)
+    removed_texts = remove_after_characters(non_empty_lines)
+    non_empty_lines = remove_blank_lines(removed_texts)
+
+
+    # print("Extracted Text:")
+    print(non_empty_lines)
+
+
+
